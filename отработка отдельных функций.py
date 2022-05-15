2e346e16c9a89cfd60c4dc36278a279c2135c072
pbal43 <pbal43@gmail.com>
2022-05-16 00:09:45 +0500

    save progr1

--- отработка отдельных функций.py
+++ отработка отдельных функций.py
@@ -1,13 +1,13 @@
 import os
 import subprocess
-# Сравнивает пути - чекер
-# x = 'echo %cd%'
+# # Сравнивает пути - чекер
+# x = r'echo %cd%'
 # x_x = os.system(x)
 # if x_x == 0:
 #     calling_output = subprocess.check_output(x, shell=True)
 #     ss = calling_output.decode('cp866')
-#     print (ss)
-# Отработка текста ошибки
+#     print(ss)
+# # Отработка текста ошибки
 # x = 'cd unknown'
 # x_x = os.system(x)
 # if x_x == 1:
@@ -15,26 +15,28 @@ import subprocess
 #     z = output.encode('cp1251')
 #     print(z.decode('cp866'))
 # отработка тела функции
-ld_1 = str.upper(str.strip(input('Введите наименование локального диска, где создана тестовая директория,\nявляющейся основной для тестирования - туда будет загружен результат тестирования в созданнную скриптом папку result\n')))
-checker = 'echo %cd%'
-while True:
-    fr_main_to_res_p = f'cd {ld_1}://test/main/results/'
-    m_r_p = os.system(fr_main_to_res_p)
-    if m_r_p == 0:
-        x = f'{fr_main_to_res_p} && {checker}'
-        x_x = os.system(x)
-        if x_x == 0:
-            calling_output = subprocess.check_output(x, shell=True)
-            ss = calling_output.decode('cp866')
-            print(ss)
-            z_z = r"\test\main\results"
-            print(f"{ld_1}:" + z_z)
-            break
-            if ss == f"{ld_1}://test/main/results":
-                with open(results, 'a') as m_r_p_rep:
-                    m_r_rep.write('Переход во вложенный каталог на 1 уровень успешен с указанием полного пути')
-                break
-    else:
-        with open(results, 'a') as m_r_p_rep:
-            m_r_rep.write('Баг! Переход во вложенный каталог на 1 уровень не осуществлен с указанием полного пути!')
-        break
+# ld_1 = str.upper(str.strip(input('Введите наименование локального диска, где создана тестовая директория,\nявляющейся основной для тестирования - туда будет загружен результат тестирования в созданнную скриптом папку result\n')))
+# checker = 'echo %cd%'
+# while True:
+#     fr_main_to_res_p = f'cd {ld_1}://test/main/results/'
+#     m_r_p = os.system(fr_main_to_res_p)
+#     if m_r_p == 0:
+#         x = f'{fr_main_to_res_p} && {checker}'
+#         x_x = os.system(x)
+#         if x_x == 0:
+#             calling_output = subprocess.check_output(x, shell=True)
+#             ss = calling_output.decode('cp866')
+#             print(ss)
+#             z_z = r"\test\main\results"
+#             print(f"{ld_1}:" + z_z)
+#             break
+#             if ss == f"{ld_1}://test/main/results":
+#                 with open(results, 'a') as m_r_p_rep:
+#                     m_r_rep.write('Переход во вложенный каталог на 1 уровень успешен с указанием полного пути')
+#                 break
+#     else:
+#         with open(results, 'a') as m_r_p_rep:
+#             m_r_rep.write('Баг! Переход во вложенный каталог на 1 уровень не осуществлен с указанием полного пути!')
+#         break
+
+# отработаем ошибочку
