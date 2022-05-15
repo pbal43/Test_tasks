21fa9d7178fed7ba8136bb844d9fcbbf63486202
pbal43 <pbal43@gmail.com>
2022-05-15 14:18:19 +0500

    save progr1

--- отработка отдельных функций.py
+++ отработка отдельных функций.py
@@ -6,11 +6,15 @@ x_x = os.system(x)
 if x_x == 0:
     calling_output = subprocess.check_output(x, shell=True)
     ss = calling_output.decode('cp866')
-    print(ss)
+    print (ss)
 # Отработка текста ошибки
-# x = 'cd unknown'
-# x_x = os.system(x)
-# if x_x == 1:
-#     output = subprocess.getoutput(x)
-#     z = output.decode('cp1251')
-#     print(z.decode('cp866'))
+x = 'cd unknown'
+x_x = os.system(x)
+if x_x == 1:
+    output = subprocess.getoutput(x)
+    z = output.encode('cp1251')
+    print(z.decode('cp866'))
+    # print(output)
+    # calling_output = subprocess.check_output(x)
+    # ss = calling_output.decode('cp866')
+    # print (ss)
